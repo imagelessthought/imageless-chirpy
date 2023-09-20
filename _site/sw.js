eb9d79668f3d7da8ebb6eb3f217d00b841dc778c
Michael Baggett <baggett.michael@gmail.com>
2023-09-20 16:33:52 -0500

    Bulbs are back

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230919.063028';
+const cacheName = 'chirpy-20230920.163303';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
