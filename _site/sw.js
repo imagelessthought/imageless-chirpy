01cd34f2739dbc54ab48fc95d3d8e59f9c763919
Michael Baggett <baggett.michael@gmail.com>
2023-09-20 17:17:50 -0500

    Bulbs are back-transitions

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230920.163303';
+const cacheName = 'chirpy-20230920.171729';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
