430960604892989a7f31a01d3a85256ec6efd5d3
Michael Baggett <baggett.michael@gmail.com>
2023-09-21 08:45:25 -0500

    Digital Signage Bulb 2

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230921.084139';
+const cacheName = 'chirpy-20230921.084511';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
