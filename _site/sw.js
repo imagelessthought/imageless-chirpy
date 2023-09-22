3f801bb476069cf386a2c9a587ef85d3a6396364
Michael Baggett <baggett.michael@gmail.com>
2023-09-22 08:50:22 -0500

    Fixed avatar and logo

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230921.232216';
+const cacheName = 'chirpy-20230922.084958';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
