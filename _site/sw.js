aecdd57e22c8b0aaa3e809a636db19817a68d539
Michael Baggett <baggett.michael@gmail.com>
2023-09-27 13:50:51 -0500

    updated link color recently updated

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230927.121049';
+const cacheName = 'chirpy-20230927.134902';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
