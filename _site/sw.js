a26d55f338f76be7aec8984670ec112e967b4960
Michael Baggett <baggett.michael@gmail.com>
2023-09-28 16:34:41 -0500

    Menu Mockups for signage added - update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230928.162730';
+const cacheName = 'chirpy-20230928.163408';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
