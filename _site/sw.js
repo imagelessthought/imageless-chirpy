f639b24b3a934f1f092c02166e1955176d454126
Michael Baggett <baggett.michael@gmail.com>
2023-09-29 13:10:49 -0500

    mb presentation

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230929.075742';
+const cacheName = 'chirpy-20230929.120917';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
