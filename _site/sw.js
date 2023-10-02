ce30276e39b2ecd96767d6dddea2904d9253099b
Michael Baggett <baggett.michael@gmail.com>
2023-10-02 16:00:07 -0500

    Preview Images

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231002.155313';
+const cacheName = 'chirpy-20231002.155941';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
