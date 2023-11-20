1c2e5890d14dfae4a84f047870e11c40c1f86054
Michael Baggett <baggett.michael@gmail.com>
2023-11-20 11:15:48 -0600

    change baseurl to main

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/jekyll/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231120.094938';
+const cacheName = 'chirpy-20231120.110723';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
