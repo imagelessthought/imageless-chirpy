aaa08cab169296756cc1814197059909889c398e
Michael Baggett <baggett.michael@gmail.com>
2023-11-29 15:27:57 -0600

    preview-image home.scss padding .5 0 wip3

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/main/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231129.150934';
+const cacheName = 'chirpy-20231129.152725';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
